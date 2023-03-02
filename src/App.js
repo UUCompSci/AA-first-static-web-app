28ffea31145a80b20f57b9c592ed78a593907fd1
Alpay Altuntas <118206645+Alpay8@users.noreply.github.com>
2023-03-02 15:50:13 +0000

    images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import "./index";
+import "./index.css";
+import "./alpay-picture";
+
+
 
 function App() {
   const greet = 'Hello';
   const ownerName = 'Alpay';
+  const myImg = document.querySelector("img")
+  myImg.src ="alpay-picture.jpg";
+  return <div>Whatsup {greet} {ownerName}<br/>
   
-  return <div>Whatsup {greet} {ownerName}</div>;
+  </div>;
 }
 
 export default App;
